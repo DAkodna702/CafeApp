61899cec823ac0dc680f298f77ccb1a1869df68a
oaemdl <oaemdl@gmail.com>
2025-05-06 13:41:01 -0500

    android

---
+++ app/src/main/java/es/oaemdl/apkcavoshcafe/ui/Inicio.java
@@ -0,0 +1,46 @@
+package es.oaemdl.apkcavoshcafe.ui;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import es.oaemdl.apkcavoshcafe.R;
+import es.oaemdl.apkcavoshcafe.databinding.FragmentInicioBinding;
+
+public class Inicio extends Fragment {
+    FragmentInicioBinding binding;
+    View view;
+    Context context;
+    NavController navController;
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentInicioBinding.inflate( inflater, container, false );
+        return view = binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        context = getContext();
+        navController = Navigation.findNavController( view );
+
+
+    }
+
+}
